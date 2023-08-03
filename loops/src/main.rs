40aa275e495661dd46334d1df7370d0bdf8b00f8
Sergio Donato <sergiodcdn@gmail.com>
2023-08-03 12:00:39 -0300

    up cap 3 done

---
+++ loops/src/main.rs
@@ -0,0 +1,14 @@
+fn fibonacci(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+fn main(){
+    let f = fibonacci(9);
+    println!("{}", f);
+}
